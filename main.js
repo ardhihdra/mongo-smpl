ac0a76958f3d026e520610c9e05e9a1a237b267e
ardhi.rofi <ardhi.rofi@elnusa.co.id>
2021-12-22 23:34:29 +0700

    add simple CRUD

--- main.js
+++ main.js
@@ -4,42 +4,44 @@ class MongoClient {
     constructor(host, port, uname, pwd, db, opt) {
         const dbhost = host ? host: process.env.MONGO_HOST
         const dbport = port ? port: process.env.MONGO_PORT
-        const dbuname = uname ? uname: process.env.MONGO_USERNAME
-        const dbpwd = pwd ? pwd: process.env.MONGO_PASSWORD
+        const dbuname = uname ? encodeURIComponent(uname): encodeURIComponent(process.env.MONGO_USERNAME)
+        const dbpwd = pwd ? encodeURIComponent(pwd): encodeURIComponent(process.env.MONGO_PASSWORD)
         const dbdb = db ? db: process.env.MONGO_DB || "admin"
         const dbopt = opt ? opt: process.env.MONGO_OPTION || '?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
         const auth = dbuname && dbpwd ? `${dbuname}:${dbpwd}@`: ''
         const uri = `mongodb://${auth}${dbhost}:${dbport}/${dbdb}${dbopt}`
         
         this.client = new mongodb.MongoClient(uri)
-        this.db = dbdb
-    }
-
-    cek() {
-        console.log("URL", this.client)
+        this.host = dbhost
+        this.port = dbport
+        this.dbname = dbdb
+        this.uri = uri
+        this.db = null
     }
 
     async connect() {
         try {
             await this.client.connect();
             await this.client.db(this.db).command({ ping: 1 });
-            console.log("Connected successfully to :", uri);
-        } catch {
+            this.db = this.client.db(this.dbname);
+            console.log("Connected successfully to mongo :", `${this.host}:${this.port}/${this.dbname}`);
+        } catch(err) {
             // Ensures that the client will close when you finish/error
-            await this.close()
+            console.log(err)
+            await this.db.close()
         }
     }
 
     async close() {
-        console.log("Disconnected from :", uri);
-        await this.client.close();
+        await this.db.close();
+        console.log("Disconnected from :", `${this.host}:${this.port}/${this.dbname}`);
     }
 
     async findOne(collection, data) {
-        const connection = this.client.serverConfig.isConnected()
+        const connection = this.db.serverConfig.isConnected()
         if(!connection) throw new Error("Not connected to mongo server")
         try {
-            const res = await this.client.collection(collection).findOne(data).catch(err => {throw err})
+            const res = await this.db.collection(collection).findOne(data).catch(err => {throw err})
             return res
         } catch (err) {
             throw err
@@ -47,34 +49,31 @@ class MongoClient {
     }
 
     async findAll(collection, query={}, options={}, isCursor=false) {
-        const connection = this.client.serverConfig.isConnected()
-        if(!connection) throw new Error("Not connected to mongo server")
         try {
-            const res = await this.client.collection(collection).find(query, options)
-                .catch(err => {throw err})
+            const res = await this.db.collection(collection).find(query, options)
             let result = isCursor ? res: await res.toArray()
-            return res
-        } catch (err) {
-            throw err
-        }
-    }
-
-    async insertOne(collection, doc) {
-        const connection = this.client.serverConfig.isConnected()
-        if(!connection) throw new Error("Not connected to mongo server")
-        try {
-            const result = await this.client.collection(collection).insertOne(doc)
             return result
         } catch (err) {
             throw err
         }
     }
 
-    async updateAll(collection, updateDoc, options) {
-        const connection = this.client.serverConfig.isConnected()
-        if(!connection) throw new Error("Not connected to mongo server")
+    async insertOne(collection, doc) {
         try {
-            const result = await this.client.collection(collection).updateMany(query, updateDoc, options)
+            const result = await this.db.collection(collection).insertOne(doc)
+            return result
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async updateAll(collection, query, doc, options) {
+        try {
+            const updateDoc = { $set: {} }
+            for(let key in doc) {
+                updateDoc.$set[key] = doc[key]
+            }
+            const result = await this.db.collection(collection).updateMany(query, updateDoc, options)
             return result
         } catch (err) {
             throw err
@@ -82,10 +81,8 @@ class MongoClient {
     }
 
     async deleteOne(collection, doc) {
-        const connection = this.client.serverConfig.isConnected()
-        if(!connection) throw new Error("Not connected to mongo server")
         try {
-            const result = await this.client.collection(collection).deleteOne(doc)
+            const result = await this.db.collection(collection).deleteOne(doc)
             return result
         } catch (err) {
             throw err
@@ -93,26 +90,11 @@ class MongoClient {
     }
 
     async deleteMany(collection, doc) {
-        const connection = this.client.serverConfig.isConnected()
-        if(!connection) throw new Error("Not connected to mongo server")
         try {
-            const result = await this.client.collection(collection).deleteMany(doc)
-            return result
-        } catch (err) {
-            throw err
-        }
-    }
-
-    async deleteMany(collection, doc) {
-        const connection = this.client.serverConfig.isConnected()
-        if(!connection) throw new Error("Not connected to mongo server")
-        try {
-            const result = await this.client.collection(collection).deleteMany(doc)
+            const result = await this.db.collection(collection).deleteMany(doc)
             return result
         } catch (err) {
             throw err
         }
     }
 }
-
-exports.MongoClient = MongoClient
